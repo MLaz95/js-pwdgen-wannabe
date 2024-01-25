cb0c9928fa95c853e61274ba0496ec9c868b81eb
marco <lazzari.marco.ml@gmail.com>
2024-01-25 14:09:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+let color = prompt("What is your favorite color?");
+
+document.writeln(`Your password is ${firstName}${lastName}${color}23. You're welcome.`)
